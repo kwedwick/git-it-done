c62a49ffa201612f84302776d659cc0400d3d867
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-07-02 18:13:48 -0500

    coded for more than 30pg, direct user to github to search

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning")
 
 
 // pulling repo from Github API
@@ -9,6 +10,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                };
             });
         } else {
             alert("There was a problem with your request!");
@@ -51,7 +56,18 @@ var displayIssues = function(issues) {
     }
 };
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "http://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
 
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 
 
